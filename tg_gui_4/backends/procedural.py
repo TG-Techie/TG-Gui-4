f1e6199cd454e5060cf876e52dfb533015562355
TG-Techie <TGTechie01@gmail.com>
2020-06-28 21:34:55 -0400

    init commit

---
+++ tg_gui_4/backends/procedural.py
@@ -0,0 +1,273 @@
+from tg_gui_4 import *
+
+def init(   *,
+            rect,
+            text,
+            centertext,
+            char_width,
+            char_height
+        ):
+
+    unit.char_width = char_width
+    unit.char_height = char_height
+
+    widget.roundrect = rect
+    widget.text = text
+    widget.centertext = centertext
+
+@container.widgetclass
+class rect(widget):
+
+    def __init__(self, *args, radius=None, color=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if radius is None:
+            radius = unit.radius
+
+        if color is None:
+            color = color_.fill
+
+        self._radius = min(self.width//2, self.height//2, radius)
+        self._color = color
+
+    @onscreen_attribute
+    def color(self, value):
+        self._color = value
+
+    def place(self):
+        super().place()
+        self.roundrect(  self._phys_x, self._phys_y,
+                    self._phys_width, self._phys_height,
+                    self._color, radius=self._radius
+                    )
+
+@container.widgetclass
+class label(widget):
+
+    def __init__(self, *args, text=None, radius=None,
+                    color=None, text_color=None,
+                    font_size=None, margin=None,
+                    **kwargs):
+        #if margin is None:
+            #margin = 0
+        super().__init__(*args, margin=margin, **kwargs)
+
+        if text is None:
+            text = "Label{}".format(self._id)
+            if len(text) > (self._phys_width//unit.char_width):
+                text = "LBL{}".format(self._id)
+
+        if radius is None:
+            radius = unit.radius
+
+        if font_size is None:
+            font_size = unit.font_size
+
+        if color is None:
+            color = color_.background
+
+        if text_color is None:
+            text_color = color_.text
+
+        self._text = ''
+        self.text = text
+        self._color = color
+        self._text_color = text_color
+        self._radius = radius
+        self._font_size = (0, font_size)[font_size > 0]
+
+    @onscreen_attribute
+    def color(self, value):
+        self._color = value
+
+    @onscreen_attribute
+    def text_color(self, value):
+        self._text_color = value
+
+    @onscreen_attribute
+    def text(self, value):
+        if '\|' in value:
+            count = value.count('\|')
+            value = value.replace('\|', ' '*((((self._phys_width//unit.char_width) - (len(value) -2*count)))//count))
+        if self._on_screen and (len(self._text) > len(value)):
+            self.roundrect(  self._phys_x, self._phys_y,
+                        self._phys_width, self._phys_height,
+                        self._color, radius=self._radius
+                        )
+        self._text = value
+
+
+    def place(self):
+        if (not self._on_screen):
+            self.roundrect(  self._phys_x, self._phys_y,
+                        self._phys_width, self._phys_height,
+                        self._color, radius=self._radius
+                        )
+        self.centertext(self._phys_x + self._phys_width//2,
+                        self._phys_y + self._phys_height//2,
+                        self.text[0:self._phys_width//unit.char_width],
+                        color=self._text_color,
+                        background=self._color,
+                        size=self._font_size
+                        )
+        super().place()
+
+@container.widgetclass
+class button(widget):
+
+    def __init__(self, *args, text=None, radius=None,
+                    color=None, text_color=None,
+                    font_size=None, identifier=None,
+                    **kwargs
+                    ):
+
+        super().__init__(*args, **kwargs)
+
+        if text is None:
+            text = "Button{}".format(self._id)
+            if len(text) > (self._phys_width//unit.char_width):
+                text = "BTN{}".format(self._id)
+
+        if color is None:
+            color = color_.fill
+
+        if text_color is None:
+            text_color = color_.text
+
+        if radius is None:
+            radius = unit.radius
+
+        if font_size is None:
+            font_size = unit.font_size
+
+        self._text = ''
+        self.text = text
+        self._radius = min(self.width//2, self.height//2, radius)
+        self._color = color
+        self._text_color = text_color
+        self._font_size = (0, font_size)[font_size > 0]
+
+        self.identifier = identifier
+
+    @onscreen_attribute
+    def text(self, value):
+        if '\|' in value:
+            count = value.count('\|')
+            value = value.replace('\|', ' '*((((self._phys_width//unit.char_width) - (len(value) -2*count)))//count))
+        self._text = value
+
+    @onscreen_attribute
+    def color(self, value):
+        self._color = value
+
+    @onscreen_attribute
+    def text_color(self, value):
+        self._text_color = value
+
+    def place(self):
+        #FIXME: use super properly
+        if not self._pointed:
+            color = self._color
+            text_color = self._text_color
+        else:
+            color = color_.pointed_fill
+            text_color = color_.pointed_text
+        self.roundrect(  self._phys_x, self._phys_y,
+                    self._phys_width, self._phys_height,
+                    color, radius=self._radius
+                    )
+        self.centertext(self._phys_x + self._phys_width//2,
+                        self._phys_y + self._phys_height//2,
+                        self.text[0:self._phys_width//unit.char_width],
+                        color=text_color,
+                        background=color,
+                        size=self._font_size
+                        )
+        super().place()
+
+    def tap(self, *args):
+        pass
+
+    def pointdown(self):
+        super().pointdown()
+        if self._on_screen:
+            self.place()
+
+    def pointup(self):
+        super().pointup()
+        if self._on_screen:
+            self.place()
+
+@container.widgetclass
+class toggleswitch(widget):
+
+    def __init__(self, *args, state=False, locked=False,
+            on_toggle=do_nothing, **kwargs):
+        super().__init__(*args, margin=2*unit.margin, **kwargs)
+        self._state = state
+        self._locked = locked
+        self.on_toggle = on_toggle
+
+    @onscreen_attribute
+    def state(self, value):
+        self._state = bool(value)
+
+    @property
+    def locked(self):
+        return self._locked
+
+    @changes_appearance
+    def lock(self):
+        self._locked = True
+        return self._unlock
+
+    @changes_appearance
+    def _unlock(self):
+        self._locked = False
+
+    def place(self):
+        inset = self._margin
+        switch_x = self._phys_x + inset
+        switch_y = self._phys_y +  inset
+        switch_width = self._phys_width - 2*inset
+        tab_width = (switch_width - inset)//2
+        tab_height = self._phys_height - 2*inset
+        empty_width = switch_width - tab_width
+
+        if self._locked:
+            tab_color = color.lightgray
+        else:
+            if self._state:
+                tab_color = color.green
+            else:
+                tab_color = color.fill
+
+        if not self._on_screen:
+            self.roundrect(self._phys_x, self._phys_y,
+                            self._phys_width, self._phys_height,
+                            color.widget_background
+                            )
+
+        if self._state:
+            #tab
+            self.roundrect( switch_x + empty_width, switch_y,
+                            tab_width, tab_height, tab_color)
+            self.roundrect( switch_x, switch_y,
+                            empty_width, tab_height, color.darkgray)
+
+        else:
+            #tab
+            self.roundrect( switch_x, switch_y,
+                            tab_width, tab_height, tab_color)
+            self.roundrect( switch_x + tab_width, switch_y,
+                            empty_width, tab_height, color.darkgray)
+        super().place()
+
+    def toggle(self, *args, **kwargs):
+        if not self._locked:
+            self.state = not self._state
+
+    def tap(self, *args):
+        #print('tap', self, self._state)
+        self.toggle()
+        self.on_toggle(self._state)
---
+++ tg_gui_4/layouts.py
@@ -0,0 +1,262 @@
+from tg_gui_4 import *
+
+_title = None
+
+'''
+def tg_watch_register_title(tlt):
+    global _title
+    tlt.text = ''
+    _title = tlt
+
+def tg_watch_update_title(tltstr):
+    global _title
+    if _title is not None:
+        with change_appearance(_title):
+            _title.text = str(tltstr)
+'''
+
+@container.widgetclass
+class matrix(container):
+
+    rows=1
+    cols=1
+
+    def __init__(self, *args, rows=None, cols=None, _build_now=True, **kwargs):
+
+        if rows is None:
+            rows = self.rows
+        elif not (isinstance(rows, int) and (rows > 0)):
+            raise ValueError("'rows' must be a whole number of type 'int")
+
+        if cols is None:
+            cols = self.cols
+        elif not (isinstance(cols, int) and (cols > 0)):
+            raise ValueError("'cols' must be a whole number of type 'int")
+
+        self._rows = rows
+        self._cols = cols
+
+        super().__init__(*args, _build_now=False, **kwargs)
+
+        self._entries = {}
+        self._dims = (self._width//cols, self._height//rows)
+
+        positions = []
+        for y in range(rows):
+            for x in range(cols):
+                positions.append((x, y))
+
+        self._positions = tuple(positions)
+
+        if _build_now:
+            self.build()
+
+    @property
+    def positions(self):
+        return self._positions
+
+    @property
+    def entries(self):
+        return self._entries
+
+    def __getitem__(self, pos):
+        return self._entries.get(pos)
+
+    def __setitem__(self, pos, mkr):
+        positions = self._positions
+        if pos not in positions:
+            raise ValueError("'pos' must be of type 'tuple' between {} -> {}".format(positions[0], positions[-1]))
+
+        x, y = pos
+        dim_width, dim_height = self._dims
+        wid = mkr(x*dim_width, y*dim_height, dim_width, dim_height, superior=self)
+        self.entries[pos] = wid
+        self.add(wid)
+
+@container.widgetclass
+class scrollview(viewport):
+
+    sections = 0
+
+    def __init__(self, *args, source=None, sections=None,
+            navigation=Vertical, **kwargs):
+
+        if isinstance(source, (list, tuple)):
+            source = list(source)
+
+        if source is None:
+            source = []
+
+        super().__init__(*args, navigation=navigation, **kwargs)
+
+        if sections is None:
+
+            sections = self.height if self.navigation is Vertical else self.width
+            sections //= unit.base
+
+        self._sections = sections
+
+        super().append(container) # start with one blank page
+
+        if self._navigation is Vertical:
+            self._item_dims = (self.width, self.height//self._sections)
+        else:
+            self._item_dims = (self.width//self._sections, self.height)
+
+        for index, prototype in enumerate(source):
+            if not (isinstance(prototype, widget_prototype) or issubclass(prototype, widget)):
+                raise ValueError(f"item at index {index} in source list is not a widget prototype or widgetclass")
+            else:
+                self.append(prototype)
+
+    @property
+    def sections(self):
+        return self._sections
+
+    @property
+    def direction(self):
+        return self._direction
+
+    def __getitem__(self, index):
+        sections = self._sections
+        return self._subviews[index//sections]._subordinates[index%sections]
+
+    def append(self, mkr):
+        if len(self._subviews[-1]._subordinates) == self._sections:
+            super().append(container)
+
+        curview = self._subviews[-1]
+        curview_subs = curview._subordinates
+
+        #self._layout()
+        if len(curview_subs) == 0:
+            wid = mkr(0, 0, *self._item_dims)
+            curview.add(wid)
+        else:
+            if self._navigation is Vertical:
+                wid = mkr(0, below(curview_subs[-1]), *self._item_dims)
+            else:
+                wid = mkr(rightof(curview_subs[-1]), 0, *self._item_dims)
+            curview.add(wid)
+        return curview_subs[-1]
+
+
+@container.widgetclass
+class navigationlink(container._button_type):
+
+    def __init__(self, *args, destination=None, **kwargs):
+
+        if not (isinstance(destination, type)\
+                and issubclass(destination, (container, viewport))
+                ):
+            if destination is None:
+                raise ValueError("'destination' not specified for {}".format(self))
+            else:
+                raise ValueError("'destination' must be a subclass of type 'container' or 'viewport'")
+
+
+        if hasattr(destination, 'name'):
+            text = destination.name
+        else:
+            text = destination.__name__
+
+        super().__init__(*args, text=text+'\|>', **kwargs)
+
+        self._destination = destination
+
+    @property
+    def destination(self):
+        return self._destination
+
+    def tap(self, *args):
+        #self.the_matrix.the_navigation_view.switchview()
+        #print('navview tap', self.superior, self.superior.superior)
+        #print(self.superior.superior.superior._layout())
+        '''the usual layout:
+        <NavView id:32> (3)
+            <scrollview 'menu' id:41> (2)
+                    <container id:42> (1)
+                            <navigationlink 'this wiget ie self' id:46>
+                            <navigationlink id:47>
+            <Destination page id:33>
+                    <label id:36>
+                    <label id:34>
+                    <label id:35>
+        '''
+        #old implementation:
+            ##       (1)     (2)      (3)
+            #self.superior.superior.superior.switchview(self._destination)
+
+        nav_view = self.superior.superior.superior
+        while not isinstance(nav_view, navigationview):
+            if nav_view is None: # not nexted in NavView
+                return
+            else:
+                nav_view = nav_view.superior
+        nav_view.switchview(self._destination)
+
+
+@widgetclass
+class navigationview(viewport):
+
+    def layout(self, menu):
+        if self._subview_index is None:
+            self._subview_index = 0
+
+        for cls in self._subviewclasses:
+            menu.append(proto.navigationlink(destination=cls))
+
+    def __init__(self, *args, name=None, _layout_now=True, **kwargs):
+
+        if hasattr(self, 'rows'):
+            rows = self.rows
+        else:
+            rows = None
+
+        super().__init__(*args, **kwargs)
+
+        if name is None:
+            if hasattr(self, 'name'):
+                name = self.name
+            else:
+                name = self.__class__.__name__
+        elif not isinstance(name, str):
+            raise ValueError("'name' must be of type 'str'")
+
+        self.enable_navigation = False
+        self._menu = menu = scrollview(0, 0, self.width, self.height,
+                                     sections=rows, superior=self
+                                    )
+        menu.name = name
+        self._subviews.insert(0, menu)
+        if _layout_now:
+            self.layout(menu)
+
+    def index(self, target):
+        if (target in self._subviewclasses):
+            return super().index(target)+1
+        else:
+            return super().index(target)
+
+    @property
+    def menu(self):
+        return self._menu
+
+    def _menu_category_tap(self, inst, *args):
+        #print(inst.text, inst.identifier)
+        self.switchview(inst.identifier)
+
+    def back(self, *args):
+        self.switchview(0)
+
+    @property
+    def priorview_active(self):
+        return self._view_index != 0
+
+    def concede(self, debug=False):
+        if debug:
+            print(self, self._subview_index, self.superior, self.subview, self.menu)
+        if self.subview != self._menu:
+            self.switchview(self._menu)
+        elif isinstance(self.superior, navigationview):
+            self.superior.priorview()
